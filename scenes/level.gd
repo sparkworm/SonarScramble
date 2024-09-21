7bd38f0fce02a19ca6ca6cb007358a2418e46a2d
Sparkworm <d.clutter2337@gmail.com>
2024-09-21 14:45:04 -0500

    Added WinZone to Level

--- scenes/level.gd
+++ scenes/level.gd
@@ -2,4 +2,15 @@
 class_name Level
 extends GameScene
 
-@export var level_name: String
+## The player
+@onready var player_submarine = $PlayerSubmarine
+## The name of the level
+@export var level_name: String = "Unnamed level"
+## The level that will be loaded when this one is won
+@export var next_level: PackedScene
+
+## called when something enters the win zone
+func _on_win_area_body_entered(body):
+	if body == player_submarine:
+		print("level won!")
+		MessageBus.game_scene_changed.emit(next_level)
