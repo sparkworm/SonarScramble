90392fffbfdd7e113551bec43395607ed24ca2f6
Sparkworm <imnota.squirp@gmail.com>
2024-09-27 14:18:56 -0500

    Added menu keybind, and made the credits return to the main menu when menu is pressed

--- scenes/transitional/credits.gd
+++ scenes/transitional/credits.gd
@@ -4,3 +4,7 @@ var main_menu = preload("res://scenes/transitional/main_menu.tscn")
 
 func quit_to_menu() -> void:
 	MessageBus.game_scene_changed.emit(main_menu)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("menu"):
+		quit_to_menu()
