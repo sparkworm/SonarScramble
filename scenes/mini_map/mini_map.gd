1ef71e7a6eaf4159535cc4f8fc5f942949d1ab40
Sparkworm <d.clutter2337@gmail.com>
2024-09-27 06:08:44 -0500

    Added minimap.  Might change this in the future to arrows that point in the direction of destinations.

---
+++ scenes/mini_map/mini_map.gd
@@ -0,0 +1,24 @@
+extends TextureRect
+
+## the amount of time that is waited between updates
+@export var update_wait_time: float = 0.1
+@export_group("Display")
+@export var player: Node2D
+@export var destination: Node2D
+@export var scale_factor: float = 10
+#@export var other_locations: Array[Node2D] # currently unused
+
+@onready var player_icon: Sprite2D = $MiniMap/Player
+@onready var destination_icon: Sprite2D = $MiniMap/Destination
+@onready var update_timer: Timer = $UpdateTimer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	update_timer.wait_time = update_wait_time
+	update_timer.start()
+	_on_update_timer_timeout()
+
+func _on_update_timer_timeout():
+	player_icon.position = player.position / scale_factor
+	destination_icon.position = destination.position / scale_factor
+	
