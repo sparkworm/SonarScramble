8aa47407d1c941dd76621bc54a9894259ff40fd5
Sparkworm <d.clutter2337@gmail.com>
2024-09-28 21:18:21 -0500

    Made the SonarDisplay a control node and added a custom panel texture.

--- scenes/sonar/sonar_display.gd
+++ scenes/sonar/sonar_display.gd
@@ -1,11 +1,13 @@
 class_name SonarDisplay
-extends Node2D
+extends Control
 
 @export var sonar: Sonar
 @export var blip_scene: PackedScene
 @export var display_radius: int = 48
 @export var scale_factor: float = 3.0
 @export var blip_lifetime: float = 2.0
+
+@onready var display = %Display
 
 func _ready():
 	sonar.blip_detected.connect(Callable(self, "draw_blip"))
@@ -15,6 +17,6 @@ func draw_blip(pos: Vector2) -> void:
 		var new_blip: Blip = blip_scene.instantiate()
 		new_blip.life_time = blip_lifetime
 		new_blip.position = Vector2i(pos / scale_factor)
-		add_child(new_blip)
+		display.add_child(new_blip)
 	else:
 		print("blip outside of screen")
